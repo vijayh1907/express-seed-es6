b787a239b12a6c06d1df58b3e1ccdda4c8b30337
ashwin-sekhar <ashwin.sekhar@people10.com>
2017-08-01 13:13:54 +0530

    display conditions tweaked

--- client/starter-app/src/app/dashboard/transaction-history/transaction-history.component.ts
+++ client/starter-app/src/app/dashboard/transaction-history/transaction-history.component.ts
@@ -3,21 +3,21 @@ import { HistoryService } from './history.service';
 
 
 
-// export class History{
-//   transaction_id:number;
-//   sender_name:string;
-//   amount:number;
-//   transaction_type:number;  //defines the type of payment(if recieved or sent)
-//   reciever_name:string;
-//   date:string;
-// }
+export class History{
+  transaction_id:number;
+  sender_name:string;
+  amount:number;
+  transaction_type:number;  //defines the type of payment(if recieved or sent)
+  receiver_name:string;
+  created_at:string;
+}
 
-// const HISTORY: History[]=[
-// {transaction_id:111,sender_name:"rishikesh",amount:10000,transaction_type:1,reciever_name:"adarash",date:"20/7/17"},
-// {transaction_id:112,sender_name:"akshay",amount:10000,transaction_type:2,reciever_name:"adarash",date:"30/1/17"},
-// {transaction_id:113,sender_name:"adarsh",amount:10000,transaction_type:2,reciever_name:"akshay",date:"1/7/17"},
-// {transaction_id:114,sender_name:"adarsh",amount:10000,transaction_type:1,reciever_name:"rishikesh",date:"4/9/17"}
-// ];
+const HISTORY: History[]=[
+{transaction_id:111,sender_name:"rishikesh",amount:10000,transaction_type:1,receiver_name:"adarash",created_at:"20/7/17"},
+{transaction_id:112,sender_name:"akshay",amount:10000,transaction_type:2,receiver_name:"adarash",created_at:"30/1/17"},
+{transaction_id:113,sender_name:"adarsh",amount:10000,transaction_type:2,receiver_name:"akshay",created_at:"1/7/17"},
+{transaction_id:114,sender_name:"adarsh",amount:10000,transaction_type:1,receiver_name:"rishikesh",created_at:"4/9/17"}
+];
 
 
 
@@ -28,22 +28,24 @@ import { HistoryService } from './history.service';
 })
 export class TransactionHistoryComponent implements OnInit {
   title='Transaction History';
-  History : any;
+  History=HISTORY;
    selectedHistory: History;
+
 
   // onSelectHistory(history:History){
   // this.selectedHistory=history;
   // };
   
-  loadTransaction() {
-      this.historyService.getTransactions()
-        .then(data => {this.History = data.transaction; console.log(this.History)});
-    }
+  // loadTransaction() {
+  //     this.historyService.getTransactions()
+  //       .then(data => {this.History = data.transaction; console.log(this.History)});
+  //   }
 
   constructor( private historyService : HistoryService) { }
 
   ngOnInit() {
-    this.loadTransaction();
+    // this.loadTransaction();
+    console.log(HISTORY);
   }
 
 }
